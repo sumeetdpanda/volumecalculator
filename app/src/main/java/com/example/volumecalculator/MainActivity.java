f943ad0fb09d9d933d786abd0bc17e19653a77d8
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-05-24 17:37:01 -0400

    UI Changes and Adapter Add

--- app/src/main/java/com/example/volumecalculator/MainActivity.java
+++ app/src/main/java/com/example/volumecalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.volumecalculator;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    ArrayList<Shape> shapesArrayList;
+    ShapeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        gridView = findViewById(R.id.gridView);
+
+        shapesArrayList = new ArrayList<>();
+
+        Shape s1 = new Shape(R.drawable.sphere, "Sphere");
+        Shape s2 = new Shape(R.drawable.cylinder, "Cylinder");
+        Shape s3 = new Shape(R.drawable.cube, "Cube");
+        Shape s4 = new Shape(R.drawable.prism, "Prism");
+
+        shapesArrayList.add(s1);
+        shapesArrayList.add(s2);
+        shapesArrayList.add(s3);
+        shapesArrayList.add(s4);
+
+        adapter = new ShapeAdapter(getApplicationContext(), shapesArrayList);
+
+        gridView.setAdapter(adapter);
+
     }
 }
