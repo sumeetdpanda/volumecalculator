cd4692501362d6dcbbc90aa4ea78aa98735c8b46
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-05-24 17:09:52 -0400

    Shape Model

---
+++ app/src/main/java/com/example/volumecalculator/Shape.java
@@ -0,0 +1,27 @@
+package com.example.volumecalculator;
+
+public class Shape {
+    private int shapeImg;
+    private String shapeName;
+
+    public Shape(int shapeImg, String shapeName) {
+        this.shapeImg = shapeImg;
+        this.shapeName = shapeName;
+    }
+
+    public int getShapeImg() {
+        return shapeImg;
+    }
+
+    public void setShapeImg(int shapeImg) {
+        this.shapeImg = shapeImg;
+    }
+
+    public String getShapeName() {
+        return shapeName;
+    }
+
+    public void setShapeName(String shapeName) {
+        this.shapeName = shapeName;
+    }
+}
